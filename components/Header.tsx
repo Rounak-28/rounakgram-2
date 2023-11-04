88930c1f6b05387be15566cc888755864cd5adac
Rounak-28 <rounakkumbhakar2004@gmail.com>
2023-11-04 19:24:40 +0530

    started adding clerk auth

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,24 @@
-const Header = () => {
+import { UserButton, currentUser } from "@clerk/nextjs";
+
+const Header = async () => {
+  // const user = await currentUser();
+  // console.log(user);
   return (
     <div className="bg-gray-200 h-16 w-screen sticky top-0 flex items-center">
       <span className="px-4">rounakgram</span>
       <button className="bg-blue-500 h-10 px-1 rounded-md absolute right-24">
         create post
       </button>
-      <div className="dp w-12 h-12 bg-red-600 rounded-full absolute right-4 cursor-pointer"></div>
+      <div className="absolute right-4">
+        <UserButton
+          afterSignOutUrl="/"
+          appearance={{
+            elements: {
+              avatarBox: "rounded-full w-12 h-12 ",
+            },
+          }}
+        />
+      </div>
     </div>
   );
 };
