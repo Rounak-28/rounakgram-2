68c7ab2649aff4750948d26ab79b49debe4fde65
Rounak-28 <rounakkumbhakar2004@gmail.com>
2023-11-05 18:36:10 +0530

    minor work on posts

--- components/Post.tsx
+++ components/Post.tsx
@@ -5,9 +5,9 @@ type PostProps = {
 
 const Post = ({ caption, imageUrl }: PostProps) => {
   return (
-    <div className="w-96 min-h-[100px] border-2 border-red-600 mx-auto my-4 rounded-lg overflow-hidden">
+    <div className="w-96 min-h-[100px] border-[1px] border-[#ffffff41] mx-auto my-4 rounded-lg overflow-hidden">
       <div className="top flex items-center space-x-3 mx-1 h-16">
-        <div className="dp bg-black w-12 h-12 rounded-full"></div>
+        <div className="dp bg-white w-12 h-12 rounded-full"></div>
         <div className="flex flex-col">
           <span>Rounak</span>
           <span className="text-sm">12 hours ago</span>
