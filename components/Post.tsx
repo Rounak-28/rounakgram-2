291492d844c54869249736b6aac9b379252f4e4f
Rounak-28 <rounakkumbhakar2004@gmail.com>
2023-11-13 07:30:26 +0530

    made the post card bigger and better

--- components/Post.tsx
+++ components/Post.tsx
@@ -9,25 +9,37 @@ type PostProps = {
   userDP: string;
 };
 
-const Post = ({ caption, imageUrl, createdAt, userName, userDP }: PostProps) => {
+const Post = ({
+  caption,
+  imageUrl,
+  createdAt,
+  userName,
+  userDP,
+}: PostProps) => {
   const time = formatDistance(new Date(createdAt), new Date(), {
     addSuffix: true,
   });
 
   return (
-    <div className="w-96 min-h-[100px] border-[1px] border-[#ffffff41] mx-auto my-4 rounded-lg overflow-hidden">
-      <div className="top flex items-center space-x-3 mx-1 h-16">
+    <div className="w-[700px] min-h-[100px] border-[1px] border-[#ffffff41] mx-auto my-6 rounded-lg overflow-hidden px-3 pt-1 pb-3">
+      <div className="top flex items-center space-x-3 h-16">
         <div className="dp bg-white w-12 h-12 rounded-full overflow-hidden">
-          <Image src={userDP} alt="" height={100} width={100}/>
+          <Image src={userDP} alt="" height={100} width={100} />
         </div>
         <div className="flex flex-col">
           <span>{userName}</span>
           <span className="text-sm">{time}</span>
         </div>
       </div>
-      <div className="main">
-        <span className="mx-1">{caption}</span>
-        <Image src={imageUrl} alt="" height={500} width={1000}/>
+      <div className="main space-y-2">
+        <span>{caption}</span>
+        <Image
+          src={imageUrl}
+          alt=""
+          height={500}
+          width={1000}
+          className="rounded-md"
+        />
       </div>
     </div>
   );
